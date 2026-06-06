de9b21306b4a592dcf6c374a20e5e25316cb76a3
BryanFinanci <bryanfinanci@gmail.com>
2026-06-06 14:25:02 -0300

    inicio do projeto

---
+++ src/main/java/com/bryangabriel/bytecurto/BytecurtoApplication.java
@@ -0,0 +1,13 @@
+package com.bryangabriel.bytecurto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytecurtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytecurtoApplication.class, args);
+	}
+
+}
