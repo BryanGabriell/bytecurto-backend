fa09fa411fa9cdfc182f3b2d0600ded187a4fd20
BryanFinanci <bryanfinanci@gmail.com>
2026-06-18 22:54:52 -0300

    criação de tabelas do banco de dados, USERS E LINKS

---
+++ src/main/resources/db/migration/v1__create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users(
+ id BIGSERIAL PRIMARY KEY,
+ name VARCHAR(60) NOT NULL,
+ email VARCHAR(254) UNIQUE NOT NULL,
+ password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE links(
+ id BIGSERIAL PRIMARY KEY,
+ url_original TEXT NOT NULL,
+ codigo_curto VARCHAR(10) UNIQUE NOT NULL,
+
+ user_id BIGINT,
+    CONSTRAINT fk_links_users
+                  FOREIGN KEY (user_id)
+                  REFERENCES users(id)
+                  ON DELETE CASCADE
+);
