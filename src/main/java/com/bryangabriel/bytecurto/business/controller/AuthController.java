ce60fdf558f5fb2972c68b278a24df29e7ba9917
BryanFinanci <bryanfinanci@gmail.com>
2026-07-04 13:23:32 -0300

    feat: adiciona a documentação Via Swagger no AuthController

--- src/main/java/com/bryangabriel/bytecurto/business/controller/AuthController.java
+++ src/main/java/com/bryangabriel/bytecurto/business/controller/AuthController.java
@@ -2,7 +2,13 @@ package com.bryangabriel.bytecurto.business.controller;
 
 import com.bryangabriel.bytecurto.business.dto.in.LoginRequestDTO;
 import com.bryangabriel.bytecurto.business.dto.out.LoginResponseDTO;
+import com.bryangabriel.bytecurto.infrastructure.config.SecurityConfig;
 import com.bryangabriel.bytecurto.infrastructure.security.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +20,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SecurityRequirement(name = SecurityConfig.SECURITY)
+@Tag(name = "AuthController", description = "Controlador de login de usuários")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
@@ -27,12 +35,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
- public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO){
-var dadosAutenticacao = new UsernamePasswordAuthenticationToken(loginRequestDTO.email(),loginRequestDTO.password());
-Authentication authentication = authenticationManager.authenticate(dadosAutenticacao);
+    @Operation(summary = "Realiza o login do usuário", description = "Controlador onde valida o login de usuários")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login efetuado com sucesso. Retorna o Token JWT"),
+            @ApiResponse(responseCode = "400", description = "Descrição malformada ou campos obrigatórios"),
+            @ApiResponse(responseCode = "401", description = "Email ou senha inválidos"),
+            @ApiResponse(responseCode = "429", description = "Muitas tentativas de login, Conta bloqueada"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    }
+    )
+     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO){
+        var dadosAutenticacao = new UsernamePasswordAuthenticationToken(loginRequestDTO.email(),loginRequestDTO.password());
+        Authentication authentication = authenticationManager.authenticate(dadosAutenticacao);
 
-String jwtToken = authService.gerarToken(authentication);
+        String jwtToken = authService.gerarToken(authentication);
 
-return ResponseEntity.ok(new LoginResponseDTO(jwtToken,3600L));
+        return ResponseEntity.ok(new LoginResponseDTO(jwtToken,3600L));
     }
 }
